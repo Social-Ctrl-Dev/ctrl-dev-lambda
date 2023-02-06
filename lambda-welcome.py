0c31cbb87cb0b21ec71005a17ad6cc61969afbb3
Tineo <tineo.dev@gmail.com>
2023-02-05 20:40:53 -0500

    Update lambda-welcome.py

--- lambda-welcome.py
+++ lambda-welcome.py
@@ -3,9 +3,9 @@ import os
 from twilio.rest import Client
 
 def lambda_handler(event, context):
+    message = "Bienvenido a Ctrl+Dev, conecta y comparte con la comunidad de dev. ¡Únete y explora tendencias!"
     client = Client(os.environ['ACCOUNT_SID'], os.environ['AUTH_TOKEN'])
     data = json.loads(event["body"])
-
 
     # Get phone number from event
     phone_number = data['phone_number']
@@ -15,7 +15,7 @@ def lambda_handler(event, context):
                     .create(
                         to=phone_number,
                         from_=os.environ['TWILIO_NUMBER'],
-                        body="Bienvenido a Ctrl+Dev"
+                        body=message
                     )
 
     return {
